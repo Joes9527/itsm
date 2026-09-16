2a5fdbf8f65becdf9ef7bd168549a4d5b90aebca
julian <eng_jun@hotmail.com>
2026-09-16 10:50:22 +0800

    style(readiness): avoid formatter version-dependent alignment

--- itsm-backend/router/initialization_readiness_test.go
+++ itsm-backend/router/initialization_readiness_test.go
@@ -29,7 +29,9 @@ func (readinessDriver) Open(name string) (driver.Conn, error) { return &readines
 func (*readinessConn) Prepare(string) (driver.Stmt, error) {
 	return nil, errors.New("unexpected prepare")
 }
-func (*readinessConn) Close() error { return nil }
+func (*readinessConn) Close() error {
+	return nil
+}
 func (*readinessConn) Begin() (driver.Tx, error) { return nil, errors.New("unexpected transaction") }
 
 func (c *readinessConn) QueryContext(_ context.Context, q string, _ []driver.NamedValue) (driver.Rows, error) {
